b1994ee780f242890a43433cbdac623c4e2cc50d
Scott Davey <me@sdavey.uk>
2015-06-10 16:29:52 +0100

    Updated test client to request each second

--- apps/python/testClient.py
+++ apps/python/testClient.py
@@ -1,16 +1,14 @@
 import socket
 import sys
+import time
 
 HOST, PORT = "localhost", 18149
-data = "req\n"
-
-# SOCK_DGRAM is the socket type to use for UDP sockets
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# As you can see, there is no connect() call; UDP has no connections.
-# Instead, data is directly sent to the recipient via sendto().
-sock.sendto(data.encode(), (HOST, PORT))
-received = sock.recv(1024)
-
-print ("Sent:     {}".format(data))
-print ("Received: {}".format(received.decode()))
+while (True):
+    #ctrl-c to exit
+    data = "req\n"
+    sock.sendto(data.encode(), (HOST, PORT))
+    received = sock.recv(1024)
+    print ("Received: {}".format(received.decode()))
+    time.sleep(1)
